6910c785b3d474e734854c3dabd5c6ba10951868
716 <525223688@qq.com>
2021-07-23 22:43:34 +0800

    first time
    Signed-off-by: 716 <525223688@qq.com>

---
+++ downloader/http_test.go
@@ -0,0 +1,18 @@
+package downloader
+
+import (
+	"github.com/allanpk716/fake-useragent/setting"
+	"testing"
+)
+
+func TestDownload_Get(t *testing.T) {
+	downloader := Download{
+		Delay:   setting.HTTP_DELAY,
+		Timeout: setting.HTTP_TIMEOUT,
+	}
+
+	_, err := downloader.Get("https://developers.whatismybrowser.com")
+	if err != nil {
+		t.Errorf("downloader.Get err: %v", err)
+	}
+}
